c78dd4bacdd9231ab5f41e3e037e0602079356f0
MaratXcode <kosjachok8@yandex.ru>
2024-06-09 19:28:23 +0300

    worc with user struct

--- LogInApp.V3/User.swift
+++ LogInApp.V3/User.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct User {
+    let login: String
+    let password: String
+    let person: Person
+    
+    static func getUser() -> User {
+        User(login: "", password: "", person: Person(name: "Marat", surname: "_FMK", hobby: "mini cooper r 53", aboutMe: "i love programming, it s  very interesting") )
+    }
+}
+    
+    struct Person {
+        let name: String
+        let surname: String
+        let hobby: String
+        let aboutMe: String
+        
+        var fullName: String {
+            "\(name) \(surname)"
+        }
+        
+        static func getPerson() -> Person {
+            Person (name: "Marat", surname: "_Fmk, height", hobby: "mini cooper r 53. car models and nature", aboutMe: "i love programming , its very interesting!")
+        }
+    }
+
